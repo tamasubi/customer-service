cb1a8a3de1df850aec0a73bf6932668bfe275e05
dominikdarnel <dominik.darnel@gmail.com>
2021-11-08 14:30:21 +0100

    status

--- models/complaint.rb
+++ models/complaint.rb
@@ -1,7 +1,8 @@
 class Complaint
     attr_accessor :title, :description, :top_priority
-    attr_reader :timestamp
-    
+    attr_reader :timestamp, :status
+
+    STATUSES = [:open, :in_progress, :closed].freeze
     
     def initialize(title, description, timestamp, top_priority)
         @title = title
@@ -9,7 +10,21 @@ class Complaint
         @timestamp = timestamp
         @top_priority = top_priority
     end
+    
+    def status_to_in_progress
+        @status = :in_progress
+    end
 
+    def status_to_closed
+        @status = :closed
+    end
+
+    def status=(status)
+        raise 'Invalid status' unless STATUSES.include?(status)
+
+        @status = status
+    end
+    
 
     def to_s
         "complaint: #{@title}"
@@ -23,4 +38,5 @@ end
 # puts complaint_one.description
 # puts complaint_one 
 
-
+complaint.move_to_inprogress
+complaint.move_to_closed
