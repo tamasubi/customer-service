bbefc9eb7cf6a05f612bacb8abdb84cab562d7f3
tamasubi <hidtamas@gmail.com>
2021-11-30 11:18:23 +0100

    complaints

--- customer_service.rb
+++ customer_service.rb
@@ -1,10 +1,32 @@
 require './models/complaint.rb'
 require './models/support_specialist.rb'
 
-complaint_one = Complaint.new("róka", "piros", Time.now, true)
-puts complaint_one.description
-puts complaint_one 
+require './db/complaints.rb'
+require './external_data/external_complaint_source.rb'
+
+EXTERNAL_COMPLAINTS.each do |external_complaint|
+    complaint = Complaint.new(
+        external_complaint[:title], 
+        external_complaint[:description], 
+        external_complaint[:timestamp], 
+        external_complaint[:top_priority]
+    )
+    
+    COMPLAINTS.push(complaint)
+end 
+
+puts COMPLAINTS
 
 
-support_specialis_one = SupportSpecialist.new("peter", "peter@example.com", "0670", true)
-puts support_specialis_one
+
+
+
+
+
+# complaint_one = Complaint.new("róka", "piros", Time.now, true)
+# puts complaint_one.description
+# puts complaint_one 
+
+
+# support_specialis_one = SupportSpecialist.new("peter", "peter@example.com", "0670", true)
+# puts support_specialis_one
--- external_data/external_complaint_source.rb
+++ external_data/external_complaint_source.rb
@@ -2,25 +2,29 @@ EXTERNAL_COMPLAINTS = [
   {
     title: 'something is not right',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    timestamp: 2.day.ago,
+    # timestamp: 2.days.ago,
+    timestamp: Time.now,
     top_priority: true
   },
   {
     title: 'want my money back',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    timestamp: 5.days.ago,
+    # timestamp: 5.days.ago,
+    timestamp: Time.now,
     top_priority: false
   },
   {
     title: 'piece of crap',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    timestamp: 1.weeks.ago,
+    # timestamp: 1.weeks.ago,
+    timestamp: Time.now,
     top_priority: false
   },
   {
     title: 'cable is broken',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
-    timestamp: 1.day.ago,
+    # timestamp: 1.day.ago,
+    timestamp: Time.now,
     top_priority: true
   }
 ].freeze
